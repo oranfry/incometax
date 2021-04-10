6e7a11f61fdae438b74c7e18bb8e999947833222
Oran Fry <oran@emerg.co>
2021-04-10 12:45:36 +1200

    Income Tax
    Load list of sale and expense accounts from the right place.

--- src/php/class/incometax/helper/IncometaxHelper.php
+++ src/php/class/incometax/helper/IncometaxHelper.php
@@ -2,15 +2,15 @@
 namespace incometax\helper;
 
 use blends\helper\LinetypeHelper;
-use Config;
+use BlendsConfig;
 
 class IncometaxHelper
 {
     public static function filter_to_incometax_transactions($blend)
     {
         $accounts = array_merge(
-            @Config::get()->sale_accounts ?? ['sale'],
-            @Config::get()->expense_accounts ?? ['expense']
+            @BlendsConfig::get(AUTH_TOKEN)->sale_accounts ?? ['sale'],
+            @BlendsConfig::get(AUTH_TOKEN)->expense_accounts ?? ['expense']
         );
 
         $blend->filters = array_merge(
@@ -27,7 +27,7 @@ class IncometaxHelper
 
     public static function filter_to_sale_transactions($blend)
     {
-        $accounts = @Config::get()->sale_accounts ?? ['sale'];
+        $accounts = @BlendsConfig::get(AUTH_TOKEN)->sale_accounts ?? ['sale'];
 
         $blend->filters = array_merge(
             @$blend->filters ?? [],
@@ -43,7 +43,7 @@ class IncometaxHelper
 
     public static function filter_to_expense_transactions($blend)
     {
-        $accounts = @Config::get()->expense_accounts ?? ['expense'];
+        $accounts = @BlendsConfig::get(AUTH_TOKEN)->expense_accounts ?? ['expense'];
 
         $blend->filters = array_merge(
             @$blend->filters ?? [],
