fd66f666a833402d0c0b6d2a5720c12980922c31
Oran Fry <oran@emerg.co>
2021-05-19 21:31:02 +1200

    Income Tax
    Load sale and expense accounts from jars list or fallback to default

--- src/php/class/incometax/helper/IncometaxHelper.php
+++ src/php/class/incometax/helper/IncometaxHelper.php
@@ -3,14 +3,53 @@ namespace incometax\helper;
 
 use blends\helper\LinetypeHelper;
 use BlendsConfig;
+use Blend;
+use Linetype;
 
 class IncometaxHelper
 {
-    public static function filter_to_incometax_transactions($blend)
+    static $sale_accounts;
+    static $expense_accounts;
+
+    public static function saleAccounts($token)
+    {
+        if (!isset(static::$sale_accounts)) {
+            $saleAccounts = @Blend::load($token, 'lists')->search($token, [(object)['field' => 'name', 'cmp' => '=', 'value' => 'saleaccounts']])[0];
+
+            if (!$saleAccounts) {
+                return static::$sale_accounts = ['sale'];
+            }
+
+            Linetype::load($token, 'list')->load_children($token, $saleAccounts);
+
+            static::$sale_accounts = map_objects($saleAccounts->listitems, 'item');
+        }
+
+        return static::$sale_accounts;
+    }
+
+    public static function expenseAccounts($token)
+    {
+        if (!isset(static::$expense_accounts)) {
+            $expenseAccounts = @Blend::load($token, 'lists')->search($token, [(object)['field' => 'name', 'cmp' => '=', 'value' => 'expenseaccounts']])[0];
+
+            if (!$expenseAccounts) {
+                return static::$expense_accounts = ['expense'];
+            }
+
+            Linetype::load($token, 'list')->load_children($token, $expenseAccounts);
+
+            static::$expense_accounts = map_objects($expenseAccounts->listitems, 'item');
+        }
+
+        return static::$expense_accounts;
+    }
+
+    public static function filter_to_incometax_transactions($token, $blend)
     {
         $accounts = array_merge(
-            @BlendsConfig::get(AUTH_TOKEN)->sale_accounts ?? ['sale'],
-            @BlendsConfig::get(AUTH_TOKEN)->expense_accounts ?? ['expense']
+            static::saleAccounts($token),
+            static::expenseAccounts($token)
         );
 
         $blend->filters = array_merge(
@@ -25,33 +64,29 @@ class IncometaxHelper
         );
     }
 
-    public static function filter_to_sale_transactions($blend)
+    public static function filter_to_sale_transactions($token, $blend)
     {
-        $accounts = @BlendsConfig::get(AUTH_TOKEN)->sale_accounts ?? ['sale'];
-
         $blend->filters = array_merge(
             @$blend->filters ?? [],
             [
                 (object) [
                     'field' => 'account',
                     'cmp' => '=',
-                    'value' => $accounts,
+                    'value' => static::saleAccounts($token),
                 ],
             ]
         );
     }
 
-    public static function filter_to_expense_transactions($blend)
+    public static function filter_to_expense_transactions($token, $blend)
     {
-        $accounts = @BlendsConfig::get(AUTH_TOKEN)->expense_accounts ?? ['expense'];
-
         $blend->filters = array_merge(
             @$blend->filters ?? [],
             [
                 (object) [
                     'field' => 'account',
                     'cmp' => '=',
-                    'value' => $accounts,
+                    'value' => static::expenseAccounts($token),
                 ],
             ]
         );
@@ -59,7 +94,7 @@ class IncometaxHelper
 
     public static function change_amount_to_net($blend)
     {
-        $field = filter_objects($blend->fields, 'name', 'is', 'amount')[0];
+        $field = @filter_objects($blend->fields, 'name', 'is', 'amount')[0];
 
         if (!$field) {
             return;
